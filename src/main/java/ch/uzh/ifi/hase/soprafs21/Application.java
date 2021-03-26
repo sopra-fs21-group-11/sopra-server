33232e5d4da3611f45d7b9141afd4bf94b65f671
tanzilkm <tanzilkmkm@gmail.com>
2021-03-26 17:07:52 +0100

    M2

--- src/main/java/ch/uzh/ifi/hase/soprafs21/Application.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/Application.java
@@ -22,7 +22,7 @@ public class Application {
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public String helloWorld() {
-        return "The application is running.";
+        return "The application is running..";
     }
 
     @Bean
