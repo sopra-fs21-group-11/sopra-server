d2bee7e4759cacfa1ce6b1aa8d72ac1718424820
Martin Jud <martin@fam-jud.ch>
2021-04-16 17:27:19 +0200

    Test skeleton. Finished for today...

---
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/entities/EvaluationTest.java
@@ -0,0 +1,14 @@
+package ch.uzh.ifi.hase.soprafs21.entities;
+
+import ch.uzh.ifi.hase.soprafs21.entity.Cards.Card;
+import org.junit.jupiter.api.Test;
+
+public class EvaluationTest {
+
+    @Test
+    public void evaluationTest() throws Exception {
+        
+
+
+    }
+}
